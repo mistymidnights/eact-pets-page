042a989dce8feca1997c9bc9926bd58564ad5565
mistymidnights <mistydeveloping@gmail.com>
2022-08-27 22:07:51 +0200

    form css finished

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,31 +1,54 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { API } from '../services/API';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { API } from "../services/API";
+import "./Register.css";
 
 const Register = () => {
   return (
-    <section className='register_section'>
-      <h2>Please, register :3</h2>
-      <form>
-        <label htmlFor="petName">Name</label>
-        <input type="text" id='petName' name='petName' />
-        
-        <label htmlFor="email">E-mail</label>
-        <input type="text" id='email' name='email' />
-        
-        <label htmlFor="password">Password</label>
-        <input type="password" id='password' name='password' />
-        
-        <label htmlFor="avatar">Avatar</label>
-        <input type="file" id='avatar' name='avatar' />
-        
-        <label htmlFor="type">Type</label>
-        <input type="text" id='type' name='type' />
+    <>
+      <section className="register_section">
+        <div className="form-container">
+          <div className="register-container">
+            <h2>Please, register :3</h2>
+          </div>
+          <form>
+            <label htmlFor="petName">Name</label>
+            <input type="text" id="petName" name="petName" />
 
-        <button type='submit'>Register</button>
-      </form>
-    </section>
-  )
-}
+            <label htmlFor="email">E-mail</label>
+            <input type="text" id="email" name="email" />
 
-export default Register
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" />
+
+            <label htmlFor="avatar">Avatar</label>
+            <div class="button-wrapper">
+              <span class="label">Upload File</span>
+
+              <input
+                type="file"
+                name="upload"
+                id="upload"
+                class="upload-box"
+                placeholder="Upload File"
+              />
+            </div>
+
+            <label htmlFor="type">Type</label>
+            <input type="text" id="type" name="type" />
+
+            <button type="submit">Register</button>
+          </form>
+        </div>
+        <div className="background-pet">
+          <img
+            src="https://www.kindpng.com/picc/m/588-5883044_kittens-transparent-persian-cat-cute-white-background-hd.png"
+            alt=""
+          />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Register;
