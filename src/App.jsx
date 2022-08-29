3248c35875d43dab02224c0828deea8c5bee055f
Elias Bello <eabelloc.dev@gmail.com>
2022-08-29 19:20:48 +0200

    Some-small-changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,20 +14,10 @@ import PetDetail from "./pages/PetDetail";
 import RequireAuth from "./components/RequiredAuth";
 
 function App() {
-  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
-
-  const switchTheme = () => {
-    const newTheme = theme === "light" ? "dark" : "light";
-    setTheme(newTheme);
-  }
-
   return (
     <JwtContextProvider>
-      
       <div className="App" data-theme={theme}>
         <Router>
-        <button onClick={switchTheme}>click</button>
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
